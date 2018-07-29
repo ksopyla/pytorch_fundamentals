a1d92853902b47d36cabc35eb044f7058eb025af
ksopyla <sopyla@ermlab.com>
2018-07-29 22:58:47 +0200

    fix batching lstm and finnaly understood how the tenor shapes should look like

--- simpleLstm.py
+++ simpleLstm.py
@@ -10,21 +10,21 @@ def init_inputs(num=1, seq_len=4, shape=(1, 2)):
     inputs = list()
     for i in range(num):
         # i*seq_len+(k+1)
-        inputs.append([torch.ones(shape) for k in range(seq_len)])
+        inputs.append([(i+1)*torch.ones(shape) for k in range(seq_len)])
     return inputs
 
 
-def init_hidden():
-    return (2*torch.ones(1, 1, 3), 2*torch.ones(1, 1, 3))
+def init_hidden(batch_size=1, hidden_size=3):
+    return (2*torch.ones(1, batch_size, hidden_size), 2*torch.ones(1, batch_size, hidden_size))
+
+
 
 
 lstm = nn.LSTM(2, 3)  # Input dim is 2, output dim is 3
-
-# inputs = [torch.ones(1, 2) for _ in range(4)]  # make a sequence of length 5
-inputs = init_inputs(1)
-
+inputs = init_inputs(num=1, shape=(1,2))
 # initialize the hidden state.
-hidden = init_hidden()
+hidden = init_hidden(hidden_size=3)
+
 for seq in inputs:
     for i in seq:
         # Step through the sequence one element at a time.
@@ -50,28 +50,61 @@ print(inputs2)
 inputs2 = inputs2.view(len(inputs2), 1, -1)
 print(inputs2)
 
-hidden = init_hidden()
+hidden = init_hidden(hidden_size=3)
 out, hidden = lstm(inputs2, hidden)
 print(out)
 print(hidden)
 
-inputs_batch = [[torch.ones(1, 2) for _ in range(4)],
-                [2*torch.ones(1, 2) for _ in range(4)]]
+
+# cretea 2D list of list of tensors of default shape
+# each list symbolize a sequence(time series) of items,
+# each item is tensor of shape (1,dim) eg it could be one word embeding
+inputs_batch = init_inputs(2)
 print(inputs_batch)
 
+# create 3D tensor, this is placeholder for transformed inputs
+# this tensor has shapes [batch, seq_len, seq_item_size]
+# tensor([
+#         # 1 batch
+#         [
+#  1 seq item  [ 0.,  0.],
+#  2 seq item  [ 0.,  0.],
+#  3 seq item  [ 0.,  0.],
+#  4 seq item  [ 0.,  0.]
+#         ],
+#         # 2 batch
+#         [
+#  1 seq item  [ 0.,  0.],
+#  2 seq item  [ 0.,  0.],
+#  3 seq item  [ 0.,  0.],
+#  4 seq item  [ 0.,  0.]
+#         ],
+#        ])
 input_tensor = torch.zeros(2, 4, 2)
 
+# go through the list of sequences and concat all sequence items into 2D tensor
 for i, seq in enumerate(inputs_batch):
     print(seq, i)
+    # put each 2D sequence of shape [seq_len, seq_item_size]
+    # into 3D tensor, first dim is a batch
     input_tensor[i] = torch.cat(inputs_batch[i])
-# permutate dimensions not values!!
-input_tensor.permute([1,0,2])
+# we have 3D tensor
 
-inputs3 = torch.cat(inputs_batch)
-print(inputs3)
-inputs3 = inputs3.view(len(inputs), 2, -1)
-print(inputs3)
-hidden = init_hidden()
-out, hidden = lstm(inputs3, hidden)
+# permutate dimensions in order to have [seq_len, batch, seq_item_size]
+input_tensor = input_tensor.permute([1, 0, 2])
+print(input_tensor)
+
+hidden = init_hidden(batch_size=2, hidden_size=3)
+out, hidden = lstm(input_tensor, hidden)
 print(out)
-print(hidden3)
+print(hidden)
+
+
+# inputs3 = torch.cat(inputs_batch)
+# print(inputs3)
+# inputs3 = inputs3.view(len(inputs), 2, -1)
+# print(inputs3)
+# hidden = init_hidden()
+# out, hidden = lstm(inputs3, hidden)
+# print(out)
+# print(hidden3)
