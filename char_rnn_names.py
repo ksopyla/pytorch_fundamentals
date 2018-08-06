e169b52d15365d42337cf4a4e22506132b1d4e7f
ksopyla <sopyla@ermlab.com>
2018-08-06 06:55:11 +0200

    rename the file and add training loop

--- char_rnn_names.py
+++ char_rnn_names.py
@@ -97,3 +97,106 @@ class RNN(nn.Module):
 
 n_hidden = 128
 rnn = RNN(n_letters, n_hidden, n_categories)
+
+
+# input = letterToTensor('A')
+# hidden =torch.zeros(1, n_hidden)
+
+# output, next_hidden = rnn(input, hidden)
+
+input = lineToTensor('Albert')
+hidden = torch.zeros(1, n_hidden)
+
+output, next_hidden = rnn(input[0], hidden)
+print(output)
+
+
+def categoryFromOutput(output):
+    top_n, top_i = output.topk(1)
+    category_i = top_i[0].item()
+    return all_categories[category_i], category_i
+
+print(categoryFromOutput(output))
+
+
+
+import random
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    line_tensor = lineToTensor(line)
+    return category, line, category_tensor, line_tensor
+
+for i in range(10):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    print('category =', category, '/ line =', line)
+    #print(category_tensor,line_tensor)
+
+criterion = nn.NLLLoss()
+
+
+learning_rate = 0.005
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+    rnn.zero_grad()
+
+    # iterate for each letter
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    # compute loss
+    loss = criterion(output,category_tensor)
+    loss.backward()
+
+    # Optimization 
+    # we could use ready to use class from pytorch, but in this 
+    # example we do it by hand
+    for p in rnn.parameters():
+        p.data.add_(-learning_rate, p.grad.data)
+    
+    return output, loss.item()
+
+
+import time
+import math
+
+n_iters = 100000
+print_every = 5000
+plot_every = 1000
+
+
+
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    # Print iter number, loss, name and guess
+    if iter % print_every == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+
+    # Add current loss avg to list of losses
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
