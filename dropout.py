c46a46bbec18f5e471f56be047c2710e583087be
ksopyla <sopyla@ermlab.com>
2019-01-18 14:06:17 +0100

    add dropout example

---
+++ dropout.py
@@ -0,0 +1,27 @@
+
+
+import torch.nn as nn
+import torch
+
+
+prob=0.5
+# define dropout
+drop = nn.Dropout(p=prob)
+
+# generate tensor
+rows=2
+columns=10
+input = torch.ones(rows, columns)
+print(f'input={input}')
+
+
+# with probability 'prob' change each tensor value to 0
+output = drop(input)
+
+# count how many zeros, shoud be about rows*columns*prob zeros
+# eg. 2*10*0.5 = 10
+zeros = torch.sum( output==0 )
+print(f'zeros={zeros}')
+# ohhh! Where my '1' go? Each value was scaled by prob 1/prob
+print(f'output={output}')
+
